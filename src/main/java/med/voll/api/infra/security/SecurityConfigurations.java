f66bb92fca90bba58871ed2d5ba6b7547883a9c5
André Victor Macedo Pereira <victorandre280@gmail.com>
2025-08-13 21:55:24 -0300

    Alteração no CORS da aplicacao

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -35,12 +40,36 @@ public class SecurityConfigurations {
                         .build();
     }
 
-
     @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
     return configuration.getAuthenticationManager();
 
   }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        // SOLUÇÃO: Usando setAllowedOriginPatterns para permitir wildcards (como qualquer porta).
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:*", // Wildcard que já permite qualquer porta
+                "http://localhost:9002", // Adicionado explicitamente
+                "http://localhost:5068", // Adicionado explicitamente
+                "http://192.168.100.17:*",
+                "https://vollfront.rasppi.site",
+                "https://vollmed.rasppi.site"
+        ));
+
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        // Permite que o frontend envie credenciais (como cookies ou tokens de autenticação).
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
